2b9c7d50a0996ba9c46e738c566271b2a60aa709
Danya-Mac <guskir3565@gmail.com>
2023-12-15 11:45:37 +0300

    bot is done

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent, \
+    WebAppInfo, Message
+
+API_TOKEN = '6929981160:AAFyRy8CJQpLhaqqxrm6fsX247gfgMADd_Y'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+def create_kb():
+    kb = InlineKeyboardMarkup(row_width=1)
+    mireaWebApp = WebAppInfo('https://www.mirea.ru/')
+    my_site_webapp = WebAppInfo('https://danchicic.github.io/site_for_tz/')
+
+    button = InlineKeyboardButton(text='Мирэа', web_app=mireaWebApp)
+    button1 = InlineKeyboardButton(text='Интернет магазин', web_app=my_site_webapp)
+    kb.add(button, button1)
+    return kb
+
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    bot.send_message(message.chat.id, text='Перейди на один из сайтов', reply_markup=create_kb())
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message: Message):
+    bot.send_message(message.chat.id, text='Перейди на один из сайтов', reply_markup=create_kb())
+
+
+bot.infinity_polling()
