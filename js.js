e084a81bcb02c8d0f06eb909b535b70b2cedd20b
Danchicic <guskir3565@gmail.com>
2023-12-21 00:22:52 +0300

    site is done

--- js.js
+++ js.js
@@ -1,61 +1,73 @@
+function create_card(card_index) {
+  // create div
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+
+  const buttonAdd = document.createElement("button");
+  const buttonDelete = document.createElement("button");
+
+  const counter = document.createElement("span");
+  const waste = document.createElement("div");
+
+  waste.classList.add("counter_div");
+
+  div.classList.add("cardDiv");
+
+  // img settings
+  img.setAttribute("src", "apples_img/" + card_index + ".jpg");
+  img.classList.add("card");
+  img.setAttribute("alt", "telephone");
+
+  // button add
+  buttonAdd.textContent = "add";
+  buttonAdd.classList.add("button_plus");
+  //button minus
+  buttonDelete.textContent = "-";
+  buttonDelete.classList.add("counting_button");
+  buttonDelete.style.display = "none";
+
+  //counter configure
+  counter.classList.add("count");
+  counter.textContent = 0;
+  counter.style.display = "none";
+
+  waste.appendChild(counter);
+  div.appendChild(waste);
+
+  div.appendChild(img);
+  div.appendChild(buttonAdd);
+  div.appendChild(buttonDelete);
+  document.querySelector(".cards").appendChild(div);
+}
 document.addEventListener("DOMContentLoaded", function () {
-  function create_card(card_index) {
-    // create div
-    const div = document.createElement("div");
-    const img = document.createElement("img");
-    const buttonAdd = document.createElement("button");
-    const buttonDelete = document.createElement("button");
-    const counterText = document.createElement("p");
-    const counter = document.createElement("span");
-    const waste = document.createElement('div');
-
-    waste.classList.add('counter_div')
-
-    div.classList.add("cardDiv");
-
-    // img settings
-    img.setAttribute("src", "apples_img/" + card_index + ".jpg");
-    img.classList.add("card");
-    img.setAttribute("alt", "telephone");
-
-    // button add
-    buttonAdd.textContent = "add";
-    buttonAdd.classList.add("button_plus");
-    //button minus
-    buttonDelete.textContent = "delete";
-    buttonDelete.classList.add( "button_minus");
-
-    //counter configure
-    counterText.textContent = "Всего товаров: ";
-    counter.classList.add("count");
-    counter.textContent = 0;
-
-    waste.appendChild(counterText);
-    waste.appendChild(counter);
-    div.appendChild(waste);
-
-    div.appendChild(img);
-    div.appendChild(buttonAdd);
-    div.appendChild(buttonDelete);
-    document.querySelector(".cards").appendChild(div);
-
-    buttonAdd.addEventListener("click", function () {
-      let count = parseInt(counter.textContent);
-      count++;
-     
-
-      counter.textContent = count;
-    });
-    buttonDelete.addEventListener("click", function () {
-      let count = parseInt(counter.textContent);
-      if (count > 0) {
-        count--;
-        counter.textContent = count;
-   
-      }
-    });
-  }
-  for (let i = 1; i < 18; i++) {
+  for (let i = 1; i < 17; i++) {
     create_card(i);
   }
+
+  const cards = document.querySelectorAll(".cardDiv");
+  cards.forEach((card) => {
+    const addButton = card.querySelector(".button_plus");
+    const buttonDelete = card.querySelector(".counting_button");
+    const counterSpan = card.querySelector(".count");
+    let count = 0;
+    addButton.addEventListener("click", () => {
+      count++;
+      counterSpan.textContent = count;
+      buttonDelete.style.display = "inline-block";
+      counterSpan.style.display = "inline-block";
+      addButton.textContent = "+";
+    });
+    buttonDelete.addEventListener("click", () => {
+      if (count > 0) {
+        count--;
+        counterSpan.textContent = count;
+      }
+      if (count == 0) {
+        buttonDelete.style.display = "none";
+        counterSpan.style.display = "none";
+        addButton.textContent = "add";
+      }
+    });
+  });
 });
+
